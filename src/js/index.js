343b4188e9c8adca9d367275f1d9378fcb7c80bc
hepyclan404 <hepyclan@yandex.ru>
2023-11-01 13:03:23 +0300

    Second commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import spoilers from './modules/spoilers.js';
 import sliderSwiper from './modules/swiper.js';
 import marquee from './modules/marquee.js';
 import mobileNav from './modules/mobile-nav.js';
+import dynAdapt from './modules/dynamic_adapt.js';
 
 
 marquee();
 sliderSwiper();
+dynAdapt();
 mobileNav();
